352a851cbd4f7bc3fcaf5dc020c374687dbf6e5a
Ess <yk2953795723@icloud.com  --Arch Disk>
2025-02-25 23:37:20 +0800

    通过 filesystem 解决了在不同编码的上下文环境下的数据库路径不匹配问题，修复了AdminModel的析构函数在impl被定义之前创建导致的错误

--- 4_代码/Agora/first/src/Models/AdminModel.cpp
+++ 4_代码/Agora/first/src/Models/AdminModel.cpp
@@ -4,79 +4,69 @@
 #include <Models/AdminModel.h>
 #include <sqlpp11/sqlpp11.h>
 
-struct AdminModel::Admin {
-  int id{};
-  std::string username;
-  std::string password;
-  Admin() = default;
-  Admin(const int id_, std::string username_, std::string password_)
-      : id(id_), username(std::move(username_)),
-        password(std::move(password_)) {}
-
-};
-
-
 struct AdminModel::AdminModelImpl : utils::DataBaseHelper {
-public:
-  explicit AdminModelImpl(conn_pool_ptr_type conn_pool_ptr)
-      : DataBaseHelper(), conn_pool_ptr(std::move(conn_pool_ptr)) {
-  }
+  public:
+    explicit AdminModelImpl(conn_pool_ptr_type conn_pool_ptr)
+        : DataBaseHelper(), conn_pool_ptr(std::move(conn_pool_ptr)) {}
 
-  [[nodiscard]] std::vector<Admin> get_all_admins() const {
-    auto query = [](const pooled_conn_ptr_type &conn) {
-      Admin_::Admin admin;
-      std::vector<Admin> ret;
-      for (const auto &row :
-           (*conn)(select(admin.id, admin.username, admin.password)
-                       .from(admin)
-                       .where(admin.id > 0))) {
-        ret.emplace_back(row.id, row.username, row.password);
-      }
-      return ret;
-    };
-    return execute<std::vector<Admin>>(conn_pool_ptr, query);
-  }
+    [[nodiscard]] std::vector<Admin> get_all_admins() const {
+        auto query = [](const pooled_conn_ptr_type &conn) {
+            Admin_::Admin admin;
+            std::vector<Admin> ret;
+            for (const auto &row :
+                 (*conn)(select(admin.id, admin.username, admin.password)
+                             .from(admin)
+                             .where(admin.id > 0))) {
+                ret.emplace_back(row.id, row.username, row.password);
+            }
+            return ret;
+        };
+        return execute<std::vector<Admin>>(conn_pool_ptr, query);
+    }
 
-  template <typename ColumnPtr, typename Value>
-  [[nodiscard]] std::vector<Admin> get_admin_generic_by(Value &&value) const {
-    auto query = [](const pooled_conn_ptr_type &conn, Value &&value_) {
-      Admin_::Admin admin;
-      ColumnPtr column_ptr{};
-      auto res =
-          (*conn)(select(admin.id, admin.username, admin.password)
-                      .from(admin)
-                      .where(admin.*column_ptr == std::forward<Value>(value_)));
-      std::vector<Admin> ret_admin{};
-      for (const auto &row : res) {
-        ret_admin.emplace_back(row.id, row.username, row.password);
-      }
-      return ret_admin;
-    };
-    return execute<std::vector<Admin>>(conn_pool_ptr, query,
-                                       std::forward<Value>(value));
-  }
+    template <typename ColumnPtr, typename Value>
+    [[nodiscard]] std::vector<Admin> get_admin_generic_by(Value &&value) const {
+        auto query = [](const pooled_conn_ptr_type &conn, Value &&value_) {
+            Admin_::Admin admin;
+            ColumnPtr column_ptr{};
+            auto res = (*conn)(
+                select(admin.id, admin.username, admin.password)
+                    .from(admin)
+                    .where(admin.*column_ptr == std::forward<Value>(value_)));
+            std::vector<Admin> ret_admin{};
+            for (const auto &row : res) {
+                ret_admin.emplace_back(row.id, row.username, row.password);
+            }
+            return ret_admin;
+        };
+        return execute<std::vector<Admin>>(conn_pool_ptr, query,
+                                           std::forward<Value>(value));
+    }
 
-  template <typename Table, typename Condition>
-  [[nodiscard]] std::vector<Admin>
-  get_admin_by_generic_condition(Table &&table, Condition &&condition) {
-    auto query = [](const pooled_conn_ptr_type &conn_, Table &&table_,
-                    Condition &&condition_) {
-      auto res = (*conn_)(select(all_of(table_))
-                              .from(table_)
-                              .where(std::forward<Condition>(condition_)));
-      std::vector<Admin> ret_admin{};
-      for (const auto &row : res) {
-        ret_admin.emplace_back(row.id, row.username, row.password);
-      }
-      return ret_admin;
-    };
-    return execute<std::vector<Admin>>(conn_pool_ptr, query,
-                                       std::forward<Table>(table),
-                                       std::forward<Condition>(condition));
-  }
+    template <typename Table, typename Condition>
+    [[nodiscard]] std::vector<Admin>
+    get_admin_by_generic_condition(Table &&table, Condition &&condition) {
+        auto query = [](const pooled_conn_ptr_type &conn_, Table &&table_,
+                        Condition &&condition_) {
+            auto res =
+                (*conn_)(select(all_of(table_))
+                             .from(table_)
+                             .where(std::forward<Condition>(condition_)));
+            std::vector<Admin> ret_admin{};
+            for (const auto &row : res) {
+                ret_admin.emplace_back(row.id, row.username, row.password);
+            }
+            return ret_admin;
+        };
+        return execute<std::vector<Admin>>(conn_pool_ptr, query,
+                                           std::forward<Table>(table),
+                                           std::forward<Condition>(condition));
+    }
 
-private:
-  conn_pool_ptr_type conn_pool_ptr;
+    ~AdminModelImpl() = default;
+
+  private:
+    conn_pool_ptr_type conn_pool_ptr;
 };
 
 AdminModel::AdminModel(conn_pool_ptr_type conn_pool_ptr)
@@ -84,34 +74,35 @@ AdminModel::AdminModel(conn_pool_ptr_type conn_pool_ptr)
 
 [[nodiscard]] std::vector<AdminModel::Admin>
 AdminModel::get_all_admins() const {
-  return impl->get_all_admins();
+    return impl->get_all_admins();
 }
 
 [[nodiscard]] std::vector<AdminModel::Admin>
 AdminModel::get_admin_by_id(const int id) const {
-  return impl->get_admin_generic_by<decltype(&Admin_::Admin::id)>(id);
+    return impl->get_admin_generic_by<decltype(&Admin_::Admin::id)>(id);
 }
 
 [[nodiscard]] std::vector<AdminModel::Admin>
 AdminModel::get_admin_by_username(const std::string &username) const {
-  return impl->get_admin_generic_by<decltype(&Admin_::Admin::username)>(
-      username);
+    return impl->get_admin_generic_by<decltype(&Admin_::Admin::username)>(
+        username);
 }
 
 [[nodiscard]] std::vector<AdminModel::Admin>
 AdminModel::get_admin_by_id_range(int lbound, int rbound) const {
-  if (lbound >= rbound) {
-    throw std::invalid_argument(FORMAT_LOG_STR("Invalid range"));
-  }
-  Admin_::Admin admin;
-  auto condition = (admin.id >= lbound && admin.id < rbound);
-  return impl->get_admin_by_generic_condition(admin, condition);
+    if (lbound >= rbound) {
+        throw std::invalid_argument(FORMAT_LOG_STR("Invalid range"));
+    }
+    Admin_::Admin admin;
+    auto condition = (admin.id >= lbound && admin.id < rbound);
+    return impl->get_admin_by_generic_condition(admin, condition);
 }
 
 [[nodiscard]] std::vector<AdminModel::Admin>
 AdminModel::get_admin_test() const {
-  Admin_::Admin admin;
-  auto condition = (admin.id == 1);
-  return impl->get_admin_by_generic_condition(admin, condition);
+    Admin_::Admin admin;
+    auto condition = (admin.id == 1);
+    return impl->get_admin_by_generic_condition(admin, condition);
 }
 
+AdminModel::~AdminModel() {}
--- 4_代码/Agora/first/src/main.cpp
+++ 4_代码/Agora/first/src/main.cpp
@@ -1,14 +1,20 @@
-#include <utils/database_utils.hpp>
-#include <utils/log.hpp>
+#include <Models/AdminModel.h>
+#include <future>
 #include <include/admin.h>
 #include <sqlpp11/sqlpp11.h>
+#include <thread>
+#include <utils/database_utils.hpp>
+#include <utils/log.hpp>
+#include <utils/utils.hpp>
 
 struct Admin {
     int id;
     std::string username;
     std::string password;
     Admin() = default;
-    Admin(const int id_, std::string username_, std::string password_): id(id_), username(std::move(username_)), password(std::move(password_)) {}
+    Admin(const int id_, std::string username_, std::string password_)
+        : id(id_), username(std::move(username_)),
+          password(std::move(password_)) {}
 };
 
 template <typename TableRow> struct utils::DataTypeTraits<TableRow, Admin> {
@@ -17,32 +23,36 @@ template <typename TableRow> struct utils::DataTypeTraits<TableRow, Admin> {
     }
 };
 
-
 int main() {
-    utils::ConnDefiner::conn_pool_ptr_type pool_ptr =
-        utils::get_conn_pool_ptr(true, "../../first/datas/sqlite3/admin.sqlite3");
+    auto dir = std::filesystem::current_path();
+    auto root_dir_path = utils::get_project_base_path(dir, "Agora");
+    fmt::println("Current Path: {}", root_dir_path.c_str());
+    utils::ConnDefiner::conn_pool_ptr_type pool_ptr = utils::get_conn_pool_ptr(
+        true, root_dir_path.c_str() +
+                  std::string("/first/datas/sqlite3/admin.sqlite3"));
+
     auto pooled_conn = pool_ptr->get();
 
-    Admin_::Admin admin{};
-    auto condition =
-        (admin.id > 0);
-    utils::GenericDataGetter<Admin> getter{};
-    auto res = getter.get_data(pool_ptr, admin, condition);
-    for (const auto &ad : res) {
-        fmt::print("{}\n", ad.username);
-    }
-
-    // for (int i = 0; i < 160; ++i) {
-    //   std::future res = std::async(std::launch::async, [pool_ptr]() {
-    //     const AdminModel model{pool_ptr};
-    //     const auto admins = model.get_admin_by_id_range(2, 10);
-    //     for (const auto &row : admins) {
-    //       fmt::print("{}: {}\n", row.id, row.username);
-    //     }
-    //   });
-    //   res.wait();
-    //   std::this_thread::sleep_for(std::chrono::seconds(1));
+    // Admin_::Admin admin{};
+    // auto condition = (admin.id > 0);
+    // utils::GenericDataGetter<Admin> getter{};
+    // auto res = getter.get_data(pool_ptr, admin, condition);
+    // for (const auto &ad : res) {
+    //     fmt::print("{}\n", ad.username);
     // }
+
+    for (int i = 0; i < 160; ++i) {
+        std::future res = std::async(std::launch::async, [pool_ptr]() {
+            const AdminModel model{pool_ptr};
+            std::vector<AdminModel::Admin> admins =
+                model.get_admin_by_id_range(2, 10);
+            for (auto &admin : admins) {
+                fmt::print("{}: {}\n", admin.id, admin.username);
+            }
+        });
+        res.wait();
+        // std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
     // for (int i = 0; i < 16; ++i) {
     //   std::future res = std::async(std::launch::async, [pool_ptr]() {
     //     const AdminModel model{pool_ptr};
