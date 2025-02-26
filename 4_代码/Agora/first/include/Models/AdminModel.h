cc3a69bb90fade80472bde005d69bd2a40b9e00f
Ess <yk2953795723@icloud.com  --Arch Disk>
2025-02-26 23:24:54 +0800

    对 AdminModel 进行了基本的实现（sample），后续将基于 AdminModel 作为示范、测试样本以及改经的参考项，此外，对 libraries.json 进行了更新，添加了 crow，但因未处理 asio 的以来的缘故，目前不能使用，或这将 crow 条目删除了再使用

--- 4_代码/Agora/first/include/Models/AdminModel.h
+++ 4_代码/Agora/first/include/Models/AdminModel.h
@@ -5,12 +5,13 @@
 #ifndef ADMINMODEL_H
 #define ADMINMODEL_H
 #include <include/admin.h>
-#include <utils/database_utils/database_utils.hpp>
 #include <utils/database_utils/database_query.hpp>
-
+#include <utils/database_utils/database_utils.hpp>
 
 struct AdminModel : utils::database_utils::ConnDefiner {
+    struct Admin;
     using size_type = size_t;
+    using data_type = std::vector<Admin>;
 
     struct Admin {
         int id;
@@ -22,26 +23,37 @@ struct AdminModel : utils::database_utils::ConnDefiner {
               password(std::move(password_)) {}
     };
 
-
     explicit AdminModel();
     explicit AdminModel(const Admin &admin) = delete;
+    // create
     size_type create(const Admin &admin) const;
-    [[nodiscard]] std::vector<Admin> get_all_admins() const;
-    [[nodiscard]] std::vector<Admin> get_admin_by_id(int id) const;
-    [[nodiscard]] std::vector<Admin>
+    // read
+    [[nodiscard]] data_type get_all_admins() const;
+    [[nodiscard]] data_type get_admin_by_id(int id) const;
+    [[nodiscard]] data_type
     get_admin_by_username(const std::string &name) const;
-    [[nodiscard]] std::vector<Admin> get_admin_by_id_range(int lbound,
-                                                           int rbound) const;
-    [[nodiscard]] std::vector<Admin> get_admin_test() const;
-    [[nodiscard]] std::vector<Admin> get_admin_by_username_test(const std::string& username) const;
+    [[nodiscard]] data_type get_admin_by_id_range(int lbound, int rbound) const;
+    [[nodiscard]] data_type get_admin_test() const;
+    [[nodiscard]] data_type
+    get_admin_by_username_test(const std::string &username) const;
+    // update
+    // if update failed return 0, else return 1
+    size_type update_by_username(const std::string &ori_username,
+                                 const Admin &admin);
+    size_type update_by_id(const std::string &ori_username, const Admin &admin);
+    // delete
+    size_type delete_by_username(const std::string &username) const;
+    size_type delete_by_id(size_type id) const;
     ~AdminModel();
 
-  // private:
+  private:
     struct AdminModelImpl;
     std::unique_ptr<AdminModelImpl> impl;
 };
 
-template <typename TableRow> struct utils::database_utils::DataTypeTraits<TableRow, AdminModel::Admin> {
+// used by GenericGetter
+template <typename TableRow>
+struct utils::database_utils::DataTypeTraits<TableRow, AdminModel::Admin> {
     static AdminModel::Admin get_args(const TableRow &row) {
         return AdminModel::Admin(row.id, row.username, row.password);
     }
