498f92808fcf8cb4c4133669f23b707b1b7cbd01
Ess <yk2953795723@icloud.com --MacOS>
2025-04-28 23:19:45 +0800

    在macos和macos上的docker容器中进行了测试，目前运行良好，在CMakeLists.txt中添加了EnableTest选项，选项用于启动单元测试，默认状态为关闭

--- 4_代码/Agora/run.sh
+++ 4_代码/Agora/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-cmake -S . -B build
-cmake --build build --parallel $(nproc) 
+cmake -S . -B build -DEnableTest=OFF # default turn off unit test
+
+ARCH=$(uname -m)
+
+case "$ARCH" in
+  x86_64|amd64) cmake --build build --parallel $(nproc);;
+  arm64|aarch64) cmake --build build --parallel 16 ;;
+  *) echo "未知架构: $ARCH"; exit 1 ;;
+esac
 # cd build/SuperMarketManagementSystem && ctest
 ./build/SuperMarketManagementSystem/SuperMarketManagementSystem
