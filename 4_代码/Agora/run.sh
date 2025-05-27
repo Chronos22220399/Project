763ffaec1f2c0d49d8accffbe1bdd9917a67c067
jd-gitee <its-a-vegetable-chicken@its-a-vegetable-chicken.com>
2025-05-27 18:14:12 +0800

    xxx

--- 4_代码/Agora/run.sh
+++ 4_代码/Agora/run.sh
@@ -20,7 +20,7 @@ cmake -DEnableTest=OFF -S . -B build -G Ninja # default turn off unit test
 ARCH=$(uname -m)
 
 case "$ARCH" in
-  x86_64|amd64) cmake --build build --parallel $(nproc);;
+  x86_64|amd64) cmake --build build --parallel 4;;
   arm64|aarch64) cmake --build build --parallel 16 ;;
   *) echo "未知架构: $ARCH"; exit 1 ;;
 esac
